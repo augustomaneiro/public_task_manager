c15f6de165a994ddad2ea92af38a39746462e208
augustomaneiro <maneiro.augusto@gmail.com>
2019-10-03 20:37:01 -0300

    ADD: emails

---
+++ src/emails/account.js
@@ -0,0 +1,26 @@
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendWelcomeMail = (email, name) =>{
+    sgMail.send({
+        to: email,
+        from: "maneiro.augusto@gmail.com",
+        subject: "Thanks for joining in",
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    });
+};
+
+const sendGoodbyeMail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: "maneiro.auusto@gmail.com",
+        subject: "Goodbye",
+        text: `We are sorry that you are leaving us, ${name}.`
+    });
+};
+
+module.exports = {
+    sendWelcomeMail,
+    sendGoodbyeMail
+};
